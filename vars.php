db233c8fd885ec46bdf652c3350ed20daa618806
Andrej Lucansky <andrejlucansky@gmail.com>
2014-11-14 17:40:53 +0100

    disponibles

--- vars.php
+++ vars.php
@@ -1,7 +1,7 @@
 <?php
 		function Conectar()
 		{
-		$conexion=mysql_connect("127.0.0.1","root","tengohambre");
+		$conexion=mysql_connect("127.0.0.1","root","root");
 		mysql_select_db("biblioteca_db", $conexion) or die("Se produjo un error en la conexion con la Base de Datos.");
 		}
 ?>
